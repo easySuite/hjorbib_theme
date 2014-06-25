5a1dfab26fe090acbfbf99167a1ca3d3f3249d4b
Andrei Bunulu <bunulu@gmail.com>
2014-06-25 10:31:18 +0300

    case 4337 - Expanded holdings section.

--- js/hjorbib.js
+++ js/hjorbib.js
@@ -2,7 +2,7 @@
   'use strict';
 
   /**
-   * Use vector logo if supported
+   * Use vector logo if supported.
    */
   Drupal.behaviors.logo = {
     attach: function (context, settings) {
@@ -12,6 +12,22 @@
       if (Modernizr.svg) {
         logo.find('a').html('<object data="' + pathToTheme + '/images/logo.svg" type="image/svg+xml"><img src="' + pathToTheme + '/logo.png"/></object>');
       }
+    }
+  };
+
+  /**
+   * Expanded holdings section by default on object page.
+   */
+  Drupal.behaviors.expandHoldings = {
+    attach: function (context) {
+      $(function () {
+        var objectPage = $('.page-ting-object', context),
+          holdingsSection = objectPage.find('.group-holdings-available').find('.field-group-format-title');
+
+        if (objectPage.length !== 0) {
+          holdingsSection.trigger('click');
+        }
+      });
     }
   };
 
