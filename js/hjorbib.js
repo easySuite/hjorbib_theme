6f8b061162cf4f83259ee099ad9f26804f699b68
Bunulu Andrei <bunulu@gmail.com>
2014-07-01 12:07:18 +0300

    develop - Changed to png logo becasue of the buggy IE.

--- js/hjorbib.js
+++ js/hjorbib.js
@@ -2,20 +2,6 @@
   'use strict';
 
   /**
-   * Use vector logo if supported.
-   */
-  Drupal.behaviors.logo = {
-    attach: function (context, settings) {
-      var logo = $('.logo', context),
-        pathToTheme = settings.basePath + settings.pathToTheme.pathToTheme;
-
-      if (Modernizr.svg) {
-        logo.find('a').html('<object data="' + pathToTheme + '/images/logo.svg" type="image/svg+xml"><img src="' + pathToTheme + '/logo.png"/></object>');
-      }
-    }
-  };
-
-  /**
    * Expanded holdings section by default on object page.
    */
   Drupal.behaviors.expandHoldings = {
