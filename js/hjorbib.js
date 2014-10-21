ded8903366abc787be4c652261d814705e01ae53
Andrei Bunulu <bunulu@gmail.com>
2014-10-21 08:18:25 +0000

    DDBEASY 223 - Scroll to previous place.

--- js/hjorbib.js
+++ js/hjorbib.js
@@ -19,10 +19,33 @@
    * Automatic scroll down to content.
    */
   $(window).load(function() {
+    // Search result page.
     if (document.location.pathname.indexOf('/search/ting/') === 0) {
-      $('html, body').animate({
-        scrollTop: $('.layout-wrapper').offset().top
-      }, 'slow');
+
+      // If back button was pressed.
+      if (window.location.hash !== 0) {
+        // id from previous page.
+        var storedId = decodeURIComponent(window.location.href).match(/\d+-\w+:\d+/)[0];
+
+        // Match stored id with id's from this page.
+        $('.search-result .heading a').each(function () {
+          var el = $(this);
+          var id = decodeURIComponent(el.attr('href')).match(/\d+-\w+:\d+/)[0];
+
+          // Scroll to item if match found.
+          if (id === storedId) {
+            $('html, body').animate({
+              scrollTop: el.closest('.search-result').offset().top
+            }, 'slow');
+          }
+        });
+      }
+      // Scroll to search results container.
+      else {
+        $('html, body').animate({
+          scrollTop: $('.layout-wrapper').offset().top
+        }, 'slow');
+      }
     }
   });
 
