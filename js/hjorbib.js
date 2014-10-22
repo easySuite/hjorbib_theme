d0d17bae6d0751b37c9420bcaaaffc2e674f4a62
Andrei Bunulu <bunulu@gmail.com>
2014-10-22 08:24:33 +0000

    DDBEASY 223 - Back button logic transfered to main theme.

--- js/hjorbib.js
+++ js/hjorbib.js
@@ -22,26 +22,8 @@
     // Search result page.
     if (document.location.pathname.indexOf('/search/ting/') === 0) {
 
-      // If back button was pressed.
-      if (window.location.hash !== '') {
-        // id from previous page.
-        var storedId = decodeURIComponent(window.location.href).match(/\d+-\w+:\d+/)[0];
-
-        // Match stored id with id's from this page.
-        $('.search-result .heading a').each(function () {
-          var el = $(this);
-          var id = decodeURIComponent(el.attr('href')).match(/\d+-\w+:\d+/)[0];
-
-          // Scroll to item if match found.
-          if (id === storedId) {
-            $('html, body').animate({
-              scrollTop: el.closest('.search-result').offset().top
-            }, 'slow');
-          }
-        });
-      }
       // Scroll to search results container.
-      else {
+      if (window.location.hash === '') {
         $('html, body').animate({
           scrollTop: $('.layout-wrapper').offset().top
         }, 'slow');
