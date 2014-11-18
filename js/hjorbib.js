d3504eae03f7f4bedd2e574f9df83a37bad4f256
Andrei Bunulu <bunulu@gmail.com>
2014-11-18 09:43:44 +0000

    DDBEASY 223 - Scroll functionality fixes.

--- js/hjorbib.js
+++ js/hjorbib.js
@@ -1,6 +1,27 @@
 (function ($) {
   'use strict';
 
+ /**
+  * History back anchor.
+  */
+  Drupal.behaviors.historyBackAnchor = {
+    attach: function (context) {
+
+      // Search result page.
+      if (document.location.pathname.indexOf('/search/ting/') === 0) {
+        $('.search-result .heading a', context).click(function() {
+          // Add hash to back button purposes to prevent scroll to top.
+          var storedId = window.location.href.match(/#back/);
+
+          if (storedId === null) {
+            window.location.href = window.location.href + '#back';
+          }
+        });
+      }
+    }
+  };
+
+
   /**
    * Expanded holdings and issues section by default on object page.
    */
@@ -15,13 +36,13 @@
     }
   };
 
-  /*
+  /**
    * Automatic scroll down to content.
    */
   $(window).load(function() {
+
     // Search result page.
     if (document.location.pathname.indexOf('/search/ting/') === 0) {
-
       // Scroll to search results container.
       if (window.location.hash === '') {
         $('html, body').animate({
